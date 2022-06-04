e6eace95ba47645d202cba1412cd20f809239285
bayamasa <mapi0213mgt@gmail.com>
2022-06-04 23:00:45 +0900

    healthcheckを追加

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main()  {
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello, World</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", healthcheck);
 	http.ListenAndServe(":8081", nil)
 }
