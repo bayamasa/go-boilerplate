4a2c1bad9fa7b9e2b1b371933330a79a92de6242
bayamasa <mapi0213mgt@gmail.com>
2024-08-06 08:12:23 +0900

    feat: healthcheck endpoint

--- server/mux.go
+++ server/mux.go
@@ -4,8 +4,8 @@ import "net/http"
 
 func NewMux() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
 	})
 	return mux
 }
